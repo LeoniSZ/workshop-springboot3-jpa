f08e9ddc1e522d5621367fc1f78fe34b8752b601
Elioenai Roberto <elioenaipro@gmail.com>
2023-10-23 17:43:38 -0300

    Order, Instant, ISO 8601 - Part 2

--- src/main/java/com/educandoweb/springcourse/entities/Order.java
+++ src/main/java/com/educandoweb/springcourse/entities/Order.java
@@ -2,7 +2,6 @@ package com.educandoweb.springcourse.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
-import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -12,24 +11,21 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
-
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private Instant moment;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
-	
-	public Order()
-	{
+
+	public Order() {
 	}
 
 	public Order(Long id, Instant moment, User client) {
@@ -65,7 +61,10 @@ public class Order implements Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -77,10 +76,11 @@ public class Order implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Order other = (Order) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-	
-	
-
 }
-	
