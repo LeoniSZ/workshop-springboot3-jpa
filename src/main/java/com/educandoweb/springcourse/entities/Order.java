210c3acfc4fd1e73e18a71fc8c7d7514ce57a8c1
Elioenai Roberto <elioenaipro@gmail.com>
2023-11-26 18:06:08 -0300

    OrderItem, many-to-many association with extra attributes PART 2

--- src/main/java/com/educandoweb/springcourse/entities/Order.java
+++ src/main/java/com/educandoweb/springcourse/entities/Order.java
@@ -2,6 +2,8 @@ package com.educandoweb.springcourse.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import com.educandoweb.springcourse.entities.enums.OrderStatus;
 
@@ -11,6 +13,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -29,6 +32,9 @@ public class Order implements Serializable {
 	@JoinColumn(name = "client_id")
 	private User client;
 
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
+	
 	public Order() {
 	}
 
@@ -76,6 +82,11 @@ public class Order implements Serializable {
 		this.client = client;
 	}
 
+	public Set<OrderItem> getItems() {
+		return items;
+	}
+	
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
